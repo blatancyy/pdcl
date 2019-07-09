36c282f9e98090668c8cbb0fa3b3f0294ed6988e
fred <fredsterdickinson@hotmail.com>
2019-07-09 01:23:56 +0100

    Added more breakpoints

--- mee6Levels.js
+++ mee6Levels.js
@@ -5,9 +5,9 @@ exports.run = async (client) => {
 
     await new Promise((resolve) => {
         for (const league of client.config.leagues) {
-            console.log(league.config.name)
+            console.log(`Beginning to load Mee6 level data for ${league.config.name}.`);
+
             if (league.config.name == "community") continue;
-    
             let id = league.config.id;
             let table = league.config.level_table;
             let url = `https://mee6.xyz/api/plugins/levels/leaderboard/${id}?page=0&limit=500`;
@@ -25,9 +25,11 @@ exports.run = async (client) => {
             });
         }
         
-        console.log('finished loop')
+        console.log("Finished looping for Mee6 level data.");
         resolve(client);
     });	
+
+    console.log("Beginning to upload Mee6 level data.");
 
 	for (const u of client.leagueUpdates) {
         db.execute(`INSERT INTO ${u.table} (id, xp) VALUES ("${u.id}", ${u.xp});`)
