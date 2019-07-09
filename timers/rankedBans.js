7fa956f99c30bc6a24b55d371451f88b3adbc889
fred <fredsterdickinson@hotmail.com>
2019-07-09 23:26:33 +0100

    Log returns in rankedBans

--- timers/rankedBans.js
+++ timers/rankedBans.js
@@ -25,9 +25,9 @@ const unban = async(client, guild, id) => {
     if (!role) return console.log(`Couldn't find role: ${roleName} whilst unbanning user from ranked in ${guild.name}.`);
 
     let member = await guild.fetchMember(id);
-    if (!member) return;
+    if (!member) return console.log("Member left the discord? Ranked unbans.");
 
-    if (!member.roles.has(role)) return;
+    if (!member.roles.has(role)) return console.log("[PDCL v3] Tried to unban user automatically, they appear to already be unbanned.");
     member.removeRole(role).catch(console.error);
     console.log(`[PDCL v3] Unbanned ${member.user.tag} from ranked in ${guild.name}.`);
 }
