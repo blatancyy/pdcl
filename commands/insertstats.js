c5f1cc26df584eb486b38bdb64a32f256594d51b
Snowful <lolitsaburrito@gmail.com>
2019-07-11 17:44:46 -0700

    insertstats updates

--- commands/insertstats.js
+++ commands/insertstats.js
@@ -1,9 +1,17 @@
 exports.aliases = ['insert']
 exports.run = async (client, message, args) => {
+	
 	let rows = message.content.split('\n');
+	// Remove map name and first team name.
+	rows.shift();
+	rows.shift();
+	// uwu so elegant
+
 	let formattedRows = [];
 	for (let row of rows) {
-		if (row.startsWith('!')) row = row.slice(row.indexOf(' '));
+		if (row.startsWith('!')) row = row.slice(row.indexOf(' ') + 1);
+		if (!row.includes('-')) continue;
+		
 		let name = row.split(' ')[0];
 		let kills = row.split(' ')[1].split('-')[0];
 		let deaths = row.split(' ')[1].split('-')[1];
