225d6171fa1137f2daaa1d39688bacd1ba7af8d1
Snowful <lolitsaburrito@gmail.com>
2019-07-12 11:12:22 -0700

    insertms.js almost done

--- commands/insertms.js
+++ commands/insertms.js
@@ -1,6 +1,12 @@
 exports.run = async (client, message, args) => {
 	
 	let formattedRows = await client.utils.get('processStats')(message.content);
+
+	message.channel.send(`Be sure to review ?insertms before using this command.\nAre you sure you want to continue? (reply yes/no)`)
+	let confirmation = await message.channel.awaitMessages(msg => msg.author.id == message.author.id, { max: 1, time: 120000, errors: ['time'] })
+		.catch(() => message.channel.send('Aborting stat insert (time ran out)'));
+	
+	if (confirmation.first().content.toLowerCase() != 'yes') return message.channel.send('Aborting stat insert (user cancelled)')
 	// Insert into DB
-	console.log('Name | Kills | Deaths\n' + formattedRows.map(r => r).join('\n'));
+	console.log('Name | Kills | Deaths\n' + formattedRows.map(r => `${r.name} | ${r.kills} | ${r.deaths}`).join('\n'));
 }
--- utils/processStats.js
+++ utils/processStats.js
@@ -4,18 +4,18 @@ module.exports = async (data) => {
 	// Remove map name and first team name.
 	rows.shift();
 	rows.shift();
-	// uwu so elegant
+	// so elegant
 
 	let formattedRows = [];
 	for (let row of rows) {
 		if (row.startsWith('!')) row = row.slice(row.indexOf(' ') + 1);
-		if (!row.includes('-')) continue;
+		if (!row.includes('-')) continue; // Ignore the second team name
 		
 		let name = row.split(' ')[0];
 		let kills = row.split(' ')[1].split('-')[0];
 		let deaths = row.split(' ')[1].split('-')[1];
-		// Now we can do whatever we want with these values, like insert to DB.
-		formattedRows.push(`${name} | ${kills} | ${deaths}`);
+
+		formattedRows.push({name, kills, deaths});
 	}
 	return formattedRows;
 }
