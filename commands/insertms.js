e92b8ff4203967fe63dbc99fb801e2017e5542bf
Snowful <lolitsaburrito@gmail.com>
2019-07-11 18:04:24 -0700

    Debugging processStats

--- commands/insertms.js
+++ commands/insertms.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
 	
-	let formattedRows = client.utils.get('processStats')(message.content);
+	let formattedRows = await client.utils.get('processStats')(message.content);
 	console.log('Name | Kills | Deaths\n' + formattedRows.map(r => r).join('\n'));
 }
