35c09067068c1989b2bc4f879c047e36e6db843a
Snowful <lolitsaburrito@gmail.com>
2019-07-11 18:59:17 -0700

    No comments

--- commands/insertms.js
+++ commands/insertms.js
@@ -1,5 +1,6 @@
 exports.run = async (client, message, args) => {
 	
 	let formattedRows = await client.utils.get('processStats')(message.content);
+	// Insert into DB
 	console.log('Name | Kills | Deaths\n' + formattedRows.map(r => r).join('\n'));
 }
