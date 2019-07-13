f8fab78eb53ae3ecdd6f57af5328cc3f6fbde3ce
fred <fredsterdickinson@hotmail.com>
2019-07-13 21:29:02 +0100

    Scope

--- utils/fetchUsername.js
+++ utils/fetchUsername.js
@@ -1,14 +1,16 @@
 module.exports = (client, uncleaned_uuid) => {
     let request = client.request;
     let uuid = clean(uncleaned_uuid);
+    var ign = null;
 
     request.get({url: `https://api.mojang.com/user/profiles/${uuid}/names`, json: true}, (e, r, b) => {
-        if (!b) return null;
-    
-        if (b.constructor.name !== "Array") return b.name;
-        return b.pop().name;
+        if (!b) return;
+        
+        if (b.constructor.name !== "Array") return ign = b.name;
+        ign = b.pop().name;
     });
 
+    return ign;
 }
 
 const clean = (uuid) => {
