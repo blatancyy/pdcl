b1e498b5f4253f2634ae01aa310e6c459540a3f8
fred <fredsterdickinson@hotmail.com>
2019-07-14 21:07:15 +0100

    Edit calculateStartElo.js

--- utils/calculateStartElo.js
+++ utils/calculateStartElo.js
@@ -1,9 +1,8 @@
-module.exports = (client) => {
-
+module.exports = async(client) => {
 	let db = client.databases.get("mscl");
-	const [rows, fields] = await db.execute(`SELECT * FROM ranked_s5 ORDER BY elo DESC LIMIT 40`)
+	const [rows, fields] = await db.execute(`SELECT * FROM ranked_s5 ORDER BY elo DESC LIMIT 40`);
 
-	for (const name of rows) {
-		client.msclElos.set(rows.displayname, (5 - Math.ceil(rows.indexOf(row) / 10.0)) * 75)
+	for (const row of rows) {
+		client.msclElos.set(rows.displayname, (5 - Math.ceil(rows.indexOf(row) / 10.0)) * 75);
 	}
 }
