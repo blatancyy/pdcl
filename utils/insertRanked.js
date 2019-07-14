0cf5ab5c030378dcb56e4271e8897ba033bbed5c
fred <fredsterdickinson@hotmail.com>
2019-07-14 22:31:10 +0100

    added insertRanked command;

---
+++ utils/insertRanked.js
@@ -0,0 +1,21 @@
+module.exports = async(client, rows) => {
+    let db = client.databases.get("mscl");
+        
+    rows.forEach(async(row) => {
+        let args = row.split(" ");
+        let ign = args[0];
+        let elo = args[1];
+
+        // https://api.mojang.com/users/profiles/minecraft/${name}?at=0 (.name)
+        let response = await client.request_promise({url: `https://api.mojang.com/users/profiles/minecraft/${ign}?at=0`, json: true});
+        if (!response) return;
+
+        let uuid = response.uuid;
+        let curIgn = await client.fetchUsername(client, uuid);
+
+        await db.execute(`INSERT INTO ranked_s5 (displayname, uuid, elo) VALUES ("${curIgn}", "${uuid}", ${elo});`)
+            .catch(console.error);
+
+        console.log(`Successfully added player ${curIgn} to the ranked_s5 table with elo: ${elo}, and uuid: ${uuid}.`);
+    });
+}
