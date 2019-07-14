5915d5ed585bd5c9095fa80d210bec2a5cc503dc
fred <fredsterdickinson@hotmail.com>
2019-07-14 23:08:58 +0100

    Tweak calculateStartElo

--- utils/calculateStartElo.js
+++ utils/calculateStartElo.js
@@ -3,6 +3,6 @@ module.exports = async(client) => {
 	const [rows, fields] = await db.execute(`SELECT * FROM ranked_s5 ORDER BY elo DESC LIMIT 40`);
 
 	for (const row of rows) {
-		client.msclElos.set(rows.displayname, (5 - Math.ceil(rows.indexOf(row) / 10.0)) * 75);
+		client.msclElos.set(row.displayname, (5 - Math.ceil(rows.indexOf(row) + 1 / 10.0)) * 75);
 	}
 }
