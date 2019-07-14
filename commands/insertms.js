138b7bce7a7b001c991795e4668bebe4dfdaa80d
fred <fredsterdickinson@hotmail.com>
2019-07-14 01:37:08 +0100

    Ranked Formula/Rework insertms

--- commands/insertms.js
+++ commands/insertms.js
@@ -1,15 +1,17 @@
-exports.run = async (client, message, args) => {
-	
-	let formattedRows = await client.processStats(message.content);
+exports.run = async (client, message, args) => {	
+	let allPlayers = await client.processStats(message.content);
+    let mappedPlayers = allPlayers.map((p) => `IGN: ${p.ign} | Rounds Played : ${p.rounds} | K: ${p.kills} | D: ${p.deaths} | W ? ${p.win} | L ? ${p.lost} | Tied ? ${p.tied} | Elo: ${p.elo} | + Elo: ${p.calculatedElo}.`);
 
-	message.channel.send(`Be sure to review ?insertms before using this command.\nAre you sure you want to continue? (reply yes/no)`)
-	let confirmation = await message.channel.awaitMessages(msg => msg.author.id == message.author.id, { max: 1, time: 120000, errors: ['time'] })
-		.catch(() => message.channel.send('Aborting stat insert (time ran out)'));
+    message.channel.send(`\`\`\`\n${mappedPlayers.join("\n")}\n\`\`\``);
+    message.channel.send("Is the above information correct? Reply yes/no.");
+
+	let confirmation = await message.channel.awaitMessages((msg) => msg.author.id == message.author.id, { max: 1, time: 120000, errors: ['time'] })
+		.catch(() => message.channel.send('Aborting stats insert. --> Time Ran Out!'));
 	
-	if (!confirmation) return;
-	if (confirmation.first().content.toLowerCase() != 'yes') return message.channel.send('Aborting stat insert (user cancelled)')
-	// Insert into DB
-	console.log('Name | Kills | Deaths\n' + formattedRows.map(r => `${r.name} | ${r.kills} | ${r.deaths}`).join('\n'));
+    if (!confirmation) return;
+    if (confirmation.first().content.toLowerCase() != 'yes') return message.channel.send('Aborting stats insert. --> User Cancelled!');
+
+    // Update into db for each player :D. We will want to catch and SEND TO CHANNEL if can't find an ign.
 }
 
 exports.help = (client, message, args) => {
@@ -17,7 +19,7 @@ exports.help = (client, message, args) => {
 
     const helpEmbed = new client.djs.RichEmbed()
     .setAuthor(client.user.tag, client.user.displayAvatarURL)
-    .addField("Description:", "Insert ranked stats for a game. **NOTE:** you should copy/paste *all* of the export content (not just the names and stats).\nMake sure each player & their stats are on their own line (see example).")
+    .addField("Description:", "Insert ranked stats for a game. **Note:** you should copy/paste **all** of the export content (not just the names and stats) purely as it is exported. Make sure each player & their stats are on their own line (see example).")
     .addField("Usage:", "`!insertms <export content>`", true)
     .addField("Example:", "`!insertms MineStrike - Inferno\nSWAT 9\nname1 18-12 +6 0.0% 1.43\nname2 19-11 +8 5.3% 1.40\nname3 16-13 +3 12.5% 1.12\n" +
 	"name4 8-12 -4 12.5% 0.69\nname5 8-15 -7 12.5% 0.62\nBombers 9\nname6 20-13 +7 0.0% 1.51\nname7 15-15 +0 6.7% 1.17\nname8 14-13 +1 14.3% 1.03\n" +
--- utils/processStats.js
+++ utils/processStats.js
@@ -1,21 +1,175 @@
-module.exports = async (data) => {
-	
+// Ideally, rows should return with properties: 
+// ign, k, d, (win/loss/draw, can be an int value ig, carry?, elo)
+
+module.exports = async(client, data) => {	
 	let rows = data.split('\n');
-	// Remove map name and first team name.
+	// Remove map name.
 	rows.shift();
-	rows.shift();
-	// so elegant
 
-	let formattedRows = [];
-	for (let row of rows) {
-		if (row.startsWith('!')) row = row.slice(row.indexOf(' ') + 1);
-		if (!row.includes('-')) continue; // Ignore the second team name
-		
+	// Split the arrays in half by finding the index of the second team name.
+	let halfwayRow = rows.find((r) => rows.indexOf(r) !== 0 && !r.includes("-"));
+	let halfwayPos = rows.indexOf(halfwayRow);
+
+	// Determine the rounds won, so can calc the winner later.
+	let team1 = rows.slice(0, halfwayPos);
+	let team2 = rows.slice(halfwayPos, rows.length);
+	let t1rounds = team1.split(" ")[1];
+	let t2rounds = team2.split(" ")[1];
+
+	let t1players = [];
+	let t2players = [];
+
+	// Round difference used in elo formula, as is win/loss.
+	let t1Won = t1rounds > t2rounds;
+	let t2Won = t2rounds > t1rounds;
+	let tie = t1rounds == t2rounds;
+	let roundDifference = t1Won ? t1rounds - t2rounds : t2rounds - t1rounds;
+	let totalRounds = t1rounds + t2rounds;
+
+	team1.forEach(async(row) => {
+		// Ignore team names:
+		if (!row.includes('-')) return;
+
 		let name = row.split(' ')[0];
-		let kills = row.split(' ')[1].split('-')[0];
-		let deaths = row.split(' ')[1].split('-')[1];
+		let k = row.split(' ')[1].split('-')[0];
+		let d = row.split(' ')[1].split('-')[1];
+		
+		// Probably a faster way to do this but: , storing as int value for ease of use when updating db.
+		let won = t1Won ? 1 : 0
+		let lost = t2Won ? 1 : 0;
+		let tied = tie ? 1 : 0;
+		let difference = won ? roundDifference : 0;
 
-		formattedRows.push({name, kills, deaths});
-	}
-	return formattedRows;
+		let elo = await client.msclElos.get(name.toLowerCase());
+		if (elo) elo == 100;
+
+		let p = {
+			ign: name,
+			kills: k,
+			deaths: d,
+			win: won,
+			loss: lost,
+			tie: tied,
+			roundDifference: difference,
+			elo: elo,
+			calculatedElo: 0,
+			rounds: totalRounds
+		}
+
+		t1players.push(p);
+	});
+
+	team2.forEach(async(row) => {
+		// Ignore team names:
+		if (!row.includes('-')) return;
+
+		let name = row.split(' ')[0];
+		let k = row.split(' ')[1].split('-')[0];
+		let d = row.split(' ')[1].split('-')[1];
+		
+		// Probably a faster way to do this but: , storing as int value for ease of use when updating db.
+		let won = t2Won ? 1 : 0
+		let lost = t1Won ? 1 : 0;
+		let tied = tie ? 1 : 0;
+		let difference = won ? roundDifference : 0;
+
+		let elo = await client.msclElos.get(name.toLowerCase());
+		if (!elo) elo = 100;
+
+		let p = {
+			ign: name,
+			kills: k,
+			deaths: d,
+			win: won,
+			loss: lost,
+			tie: tied,
+			roundDifference: difference,
+			elo: elo,
+			calculatedElo: 0,
+			rounds: totalRounds
+		}
+
+		t2players.push(p);
+	});
+
+	t1players.forEach((p) => calculateElo(p, t1players, t2players));
+	t2players.forEach((p) => calculateElo(p, t2players, t1players));
+
+	let allPlayers = t1players + t2players;
+	return allPlayers;
+}
+
+const calculateElo = (p, team, enemy) => {
+	/* 
+		(20 * roundDifferenceFactor * playerNumber * result * (1 + result * eloDifferenceFactor) 
+			* (1 + result * performanceFactor) + carryBonus + expectations)
+
+		Expectations is difficult to do. For now, I'm just going to leave it out because it's
+		rarely actually applicable. I'm going to comment each value with more explanation.
+	*/
+
+
+	// This one is pretty simple, depending on how many rounds the team won by they are given a multiplier.
+	let roundDifferenceFactor;
+	let roundDifference = p.roundDifference;
+
+	if (roundDifference >= 6) roundDifferenceFactor = 1.2;
+	else if (roundDifference < 6 && roundDifference > 3) roundDifferenceFactor = 1.05;
+	else roundDifferenceFactor = 0;
+	
+	// Halve the elo if there's less than 5 players:
+	let playerNumber = 1;
+	let playerCount = team.length;
+	if (playerCount < 5) playerNumber = 0.5;
+
+	// Result: 1 if win, 0.5 if tie, -1 if loss.
+	let result;
+	if (p.win == 1) result = 1;
+	if (p.tie == 1) result = 0.5;
+	if (p.loss == 1) result = -1;
+	
+	/* 
+		Elo difference factor:  
+		'take the (absolute value of the average of the other teams elo minus the absolute value of the 
+		average of your teams elo) / (4 * the average elo of all players);'
+								
+		This one factor means we have to store all players elo in cache, which upsets me but w/e. It's important.
+		If we use keyv to make a simple map of (name --> ign) then this will be so much easier!
+	*/							   
+
+	let t1TotalElo;
+	team.forEach((player) => t1TotalElo += player.elo);
+	let t1AvgElo = t1TotalElo / team.length;
+	
+	let t2TotalElo;
+	enemy.forEach((player) => t2TotalElo += player.elo);
+	let t2AvgElo = t2TotalElo / enemy.length;
+
+	let totalElo = t1TotalElo + t2TotalElo;
+	let totalAvgElo = totalElo / (team.length + enemy.length);
+
+	let eloDifferenceFactor = ((Math.abs(t1AvgElo) - Math.abs(t2AvgElo)) / ( 4 * totalAvgElo));
+
+	// Performance Factor: '(take the players kills per round minus his teams average kills per round) / 3;'
+	let indKillsPerRound = p.kills / p.rounds;
+	
+	let teamsKillsPerRound = [];
+	team.forEach((player) => {
+		if (player.name.toLowerCase() == p.name.toLowerCase()) return;
+		let kpr = player.kills / player.rounds;
+		teamsKillsPerRound.push(kpr);
+	});
+
+	let teamAvgKillsPerRound;
+	teamsKillsPerRound.forEach((kpr) => teamAvgKillsPerRound += kpr);
+
+	let performanceFactor = (indKillsPerRound - teamAvgKillsPerRound) / 3;
+
+	// Carry Bonus: If your KPR > 1.5 * (TeamAvgKPR), = 5;
+	let carryBonus = 0;
+	if (indKillsPerRound > (1.5 * teamAvgKillsPerRound)) carryBonus = 5;
+
+	// Finally, calculate and edit players elo:
+	let elo = (20 * roundDifferenceFactor * playerNumber * result * (1 + result * eloDifferenceFactor) * (1 + result * performanceFactor) + carryBonus);
+	return p.calculatedElo = elo;
 }
