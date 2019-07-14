bd99866a12a3e4507a226b163517db77718fb239
fred <fredsterdickinson@hotmail.com>
2019-07-14 16:06:19 +0100

    Minor change to insertms validation v2

--- commands/insertms.js
+++ commands/insertms.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args) => {
     if (message.hub) return;
 
     let league = client.config.leagues.find((l) => l.config.id == message.guild.id);
-    if (!league.name !== "mscl") return;
+    if (!league.config.name !== "mscl") return;
 
     if (!league.config.ranked.stats.includes(message.author.id)) return;
 
@@ -28,10 +28,10 @@ exports.help = (client, message, args) => {
     const helpEmbed = new client.djs.RichEmbed()
     .setAuthor(client.user.tag, client.user.displayAvatarURL)
     .addField("Description:", "Insert ranked stats for a game. **Note:** you should copy/paste **all** of the export content (not just the names and stats) purely as it is exported. Make sure each player & their stats are on their own line (see example).")
-    .addField("Usage:", "`!insertms <export content>`", true)
+    .addField("Usage:", "`!insertms <export content>`")
     .addField("Example:", "`!insertms MineStrike - Inferno\nSWAT 9\nname1 18-12 +6 0.0% 1.43\nname2 19-11 +8 5.3% 1.40\nname3 16-13 +3 12.5% 1.12\n" +
 	"name4 8-12 -4 12.5% 0.69\nname5 8-15 -7 12.5% 0.62\nBombers 9\nname6 20-13 +7 0.0% 1.51\nname7 15-15 +0 6.7% 1.17\nname8 14-13 +1 14.3% 1.03\n" +
-	"name9 10-13 -3 10.0% 0.83\nname10 4-15 -11 0.0% 0.35`", true)
+	"name9 10-13 -3 10.0% 0.83\nname10 4-15 -11 0.0% 0.35`")
     .setColor("DARK_AQUA")
     .setFooter("!insertms")
     .setTimestamp();
