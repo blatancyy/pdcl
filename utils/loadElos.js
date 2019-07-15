e9dd138b7985a3ceda030037cb1864bc271c43b6
fred <fredsterdickinson@hotmail.com>
2019-07-15 12:55:06 +0100

    Minor changes to loadElos.js

--- utils/loadElos.js
+++ utils/loadElos.js
@@ -1,6 +1,9 @@
 module.exports = async(client) => {
 	let db = client.databases.get("mscl");
-	const [rows, fields] = await db.execute(`SELECT * FROM ranked_s5 ORDER BY elo DESC LIMIT 40`);
+	const [rows, fields] = await db.execute(`SELECT * FROM ranked_s5`);
+	rows.sort((p1, p2) => p2.elo - p1.elo);
+
+	console.log(rows);
 
 	for (const row of rows) {
 		client.lastSeasonElos.set(row.displayname, (5 - Math.ceil(rows.indexOf(row) + 1 / 10.0)) * 75);
