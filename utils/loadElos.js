de4c70252f6cab6f6f21dbfc1615cc0b38b74d98
fred <fredsterdickinson@hotmail.com>
2019-07-15 12:58:02 +0100

    Bracketing elo calc

--- utils/loadElos.js
+++ utils/loadElos.js
@@ -2,11 +2,10 @@ module.exports = async(client) => {
 	let db = client.databases.get("mscl");
 	const [rows, fields] = await db.execute(`SELECT * FROM ranked_s5`);
 	rows.sort((p1, p2) => p2.elo - p1.elo);
-
-	console.log(rows);
-
+	
 	for (const row of rows) {
-		client.lastSeasonElos.set(row.displayname, (5 - Math.ceil(rows.indexOf(row) + 1 / 10.0)) * 75);
+		let elo = (5 - (Math.ceil((rows.indexOf(row) + 1) / 10))) * 75;
+		client.lastSeasonElos.set(row.displayname, elo);
 	}
 
 	const [rows2, fields2] = await db.execute(`SELECT * FROM ranked_s6;`);
