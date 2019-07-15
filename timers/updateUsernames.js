73f2f5a3a77e6edf6a6ca5e1fe186b0cfe750bfd
fred <fredsterdickinson@hotmail.com>
2019-07-15 17:48:30 +0100

    Test namechange for mscl ranked

--- timers/updateUsernames.js
+++ timers/updateUsernames.js
@@ -2,6 +2,8 @@ exports.time = 60000 * 60 * 2;
 exports.run = async(client) => {
 	for (const league of client.config.leagues) {
         let ranked = league.config.ranked.status;
+        if (!ranked) return;
+
         let rankedTable = league.config.ranked.table;
         let database = league.config.database;
 		
