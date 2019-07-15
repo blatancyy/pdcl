fc5f633b7ca49fd7bae3c62f47c6c908e268c0cd
fred <fredsterdickinson@hotmail.com>
2019-07-15 17:56:16 +0100

    Added breakpoitns

--- timers/updateUsernames.js
+++ timers/updateUsernames.js
@@ -3,18 +3,20 @@ exports.run = async(client) => {
 	for (const league of client.config.leagues) {
         let ranked = league.config.ranked.status;
         if (!ranked) return;
-
+        console.log(league);
+        
         let rankedTable = league.config.ranked.table;
         let database = league.config.database;
 		
 		client.updateUsernames(client, client.players[league.config.name], { db: database, table: "players"});
-        
+        console.log('updatedUsernames complete');
         if (ranked) {
             const db = client.databases.get(league.config.name);
             const [rows, fields] = await db.execute(`SELECT * FROM ${rankedTable};`);
+            console.log(rows);
             client.updateUsernames(client, rows, {db: database, table: rankedTable});
         }
         
-        // await client.wait(1000 * 60 * 15);
+        await client.wait(1000 * 60 * 15);
 	}
 }
--- utils/updateUsernames.js
+++ utils/updateUsernames.js
@@ -2,6 +2,8 @@ module.exports = async(client, players, dbInfo) => {
     let db = await createNewConnection(client, dbInfo.db);
     if (!db) return console.log(`Failed to create a new connection with db name: ${dbInfo.db}`);
 
+    console.log(players);
+
     players.forEach((player) => {
         let current = player.displayname;
         let uuid = clean(player.uuid);
@@ -12,7 +14,9 @@ module.exports = async(client, players, dbInfo) => {
             if (b.constructor.name !== "Array") return console.log(b);
 
             let updated = b.pop().name;
+            console.log("after mojang request" + updated);
             if (current == updated) return;
+            console.log("after check if same")
 
             console.log(`[QUERY] UPDATE ${dbInfo.table} SET displayname = "${updated}" WHERE displayname = "${current}";`)
             db.execute(`UPDATE ${dbInfo.table} SET displayname = "${updated}" WHERE displayname = "${current}";`).catch(e => console.log(e));
@@ -20,7 +24,6 @@ module.exports = async(client, players, dbInfo) => {
             console.log(`[PDCL v3][UPDATE USERNAMES] Successfully updated username in league: ${dbInfo.db} for ${dbInfo.table}, : ${current} --> ${updated}.`);
         });
     });
-
 }
 
 const clean = (uuid) => {
