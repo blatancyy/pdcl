f3c21fa6649e85541b8b9fe29375b8872365fb4d
fred <fredsterdickinson@hotmail.com>
2019-07-16 02:16:53 +0100

    Add carry to mappedPlayers

--- commands/insertms.js
+++ commands/insertms.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args) => {
     if (!league.config.ranked.stats.includes(message.author.id)) return;
 
 	let allPlayers = await client.processStats(client, message.content);
-    let mappedPlayers = allPlayers.map((p) => `IGN: ${p.ign} | Rounds Played : ${p.rounds} | K: ${p.kills} | D: ${p.deaths} | W ? ${p.win} | L ? ${p.loss} | Tied ? ${p.tie} | Elo: ${p.elo} | + Elo: ${p.calculatedElo}.\n`);
+    let mappedPlayers = allPlayers.map((p) => `IGN: ${p.ign} | Rounds Played : ${p.rounds} | Carry ? ${p.carry} | K: ${p.kills} | D: ${p.deaths} | W ? ${p.win} | L ? ${p.loss} | Tied ? ${p.tie} | Elo: ${p.elo} | + Elo: ${p.calculatedElo}.\n`);
 
     message.channel.send(`\`\`\`\n${mappedPlayers.join("\n")}\n\nby ${message.author.tag}\n\`\`\``);
     message.channel.send("Is the above information correct? Reply yes/no. If no, let @ fred#5775 know asap.");
