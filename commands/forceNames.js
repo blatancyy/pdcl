cce006f1c0667c05658bbcf53762deca1c40d30a
fred <fredsterdickinson@hotmail.com>
2019-07-17 17:28:42 +0100

    Added force name command

---
+++ commands/forceNames.js
@@ -0,0 +1,23 @@
+exports.aliases = ["checknames", "names"];
+exports.run = async(client, message, args) => {
+    if (!message.home) return;
+    if (message.hub) return;
+
+    let league = client.config.leagues.find((l) => l.config.id == message.guild.id);
+    if (!league) return console.log(`Failed to find config for league: ${message.guild.name}, yet it is a home guild?`);
+    
+    let role = message.guild.roles.find((r) => r.name.toLowerCase() == "management");
+    if (!league.config.ranked.stats.includes(message.author.id) && !message.member.roles.has(role.id)) return;
+
+    let ranked = league.config.ranked.status;         
+    let rankedTable = league.config.ranked.table;
+    let database = league.config.database;
+    if (!ranked) return;    
+
+    const db = client.databases.get(league.config.name);
+    const [rows, fields] = await db.execute(`SELECT * FROM ${rankedTable};`);
+    await client.updateUsernames(client, rows, { db: database, table: rankedTable });
+    await client.loadElos(client);
+
+    message.channel.send(`Successfully forced the update username script for ${rankedTable} in database: ${league.config.name}.`);
+}
