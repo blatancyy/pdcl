63bd59ac8580321726860a2c94a2372b929810f2
Snowful <lolitsaburrito@gmail.com>
2019-12-13 13:57:39 -0800

    One signup for ranked per discord account

--- commands/signup.js
+++ commands/signup.js
@@ -1,7 +1,12 @@
 exports.run = async(client, message, args) => {
     if (!message.home) return console.log('is not a home guild');
-    if (message.hub) return console.log('is a hub channel');
-    
+	if (message.hub) return console.log('is a hub channel');
+	
+	const discordDB = client.databases.get('discord');
+	const [signups, fields] = await discordDB.execute(`SELECT * FROM ranked_signups WHERE discord_id = "${message.author.id}" AND season = "${league.config.ranked.table}" AND league = "${message.league.id}";`)
+		.catch(e => console.error(e));
+	if (signups.length > 0) return message.channel.send(`You've already signed up an account for ranked this season.`);
+
     let league = client.config.leagues.find((l) => l.config.id == message.guild.id);
 	if (!league.config.ranked.status) return console.log('not ranked : ' + league.config.name);
 
@@ -23,7 +28,9 @@ exports.run = async(client, message, args) => {
     
     let table = league.config.ranked.table;
 	await db.execute(`INSERT INTO ${table} (displayname, uuid, elo) VALUES ("${username}", "${uuid}", ${startingElo});`).catch(console.error);
-
+	
+	await discordDB.execute(`INSERT INTO ranked_signups (discord_id, season, league_id) VALUES ("${message.author.id}", "${league.config.ranked.table}", "${message.league.id}");`)
+		.catch(e => console.error(e));
 	if (!client.players[league.config.name].some(p => p.displayname.toLowerCase().includes(username.toLowerCase()))) {
 		await db.execute(`INSERT INTO players (displayname, uuid, team_id) VALUES ("${username}", "${uuid}", 0);`);
 		client.players[league.config.name].push({ displayname: username, uuid, region: 0, team_id: 0, teamrank: 0, leaguerank: 0, kills: 0, deaths: 0, rating: 0, blacklisted: 0, token: null, games_played: 0 });	
@@ -35,6 +42,7 @@ exports.run = async(client, message, args) => {
 		playerElo = client.playerElos.get(username);
 	}
 	playerElo[league.config.name] = startingElo;
+	message.member.setNickname(username, 'Auto-set when signing up for ranked.');
     console.log(`Successfully set ${username}'s (${uuid}) starting elo to ${startingElo}.`);
 }
 
